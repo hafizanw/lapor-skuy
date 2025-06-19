05aee24a56c1849eb8e6670fa858cac6cfbb0395
NovaReXs <85391524+NovaReXs@users.noreply.github.com>
2025-06-19 11:25:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\faq_controller;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\kirim_aduan_controller;
-use App\Http\Controllers\user_profile_controller;
-use App\Http\Controllers\lihat_aduan_anda_controller;
-use App\Http\Controllers\lihat_aduan_umum_controller;
-use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\dashboard_controller;
+use App\Http\Controllers\faq_controller;
+use App\Http\Controllers\kirim_aduan_controller;
+use App\Http\Controllers\lihat_aduan_anda_controller;
 use App\Http\Controllers\Lihat_aduan_detail_controller;
-
+use App\Http\Controllers\lihat_aduan_umum_controller;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\user_profile_controller;
+use Illuminate\Support\Facades\Route;
 
 // Route untuk halaman utama tanpa login
 
@@ -30,14 +29,13 @@ Route::get('/reset/{token}', [ResetPasswordController::class, 'showResetForm'])
 Route::post('/reset', [ResetPasswordController::class, 'reset'])
     ->name('password.update');
 
-
 // Route untuk halaman utama after login
 Route::middleware(['auth'])->group(function () {
 
     Route::get('/dashboard', [dashboard_controller::class, 'index'])->name('home');
 
     Route::post('/aduan-umum', [lihat_aduan_umum_controller::class, 'store'])->name('aduan-umum');
-    
+
     Route::get('/aduan-umum', [lihat_aduan_umum_controller::class, 'index'])->name('aduan-umum');
 
     Route::get('/aduan-anda', [lihat_aduan_anda_controller::class, 'index'])->name('aduan-anda');
@@ -51,22 +49,17 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/user-profile', [user_profile_controller::class, 'store'])->name('user-profile');
 
     Route::get('/kirim-aduan', [kirim_aduan_controller::class, 'index'])->name('kirim-aduan');
-    
+
     Route::get('/kirim-aduan-umum', [kirim_aduan_controller::class, 'index_umum'])->name('kirim-aduan-umum');
 
     Route::post('/kirim-aduan-umum', [kirim_aduan_controller::class, 'store'])->name('kirim-aduan-umum.store');
-    
+
     Route::get('/kirim-aduan-privat', [kirim_aduan_controller::class, 'index_privat'])->name('kirim-aduan-privat');
 
     Route::post('/kirim-aduan-privat', [kirim_aduan_controller::class, 'store'])->name('kirim-aduan-privat.store');
 
-  
 });
 
 Route::get('/faq', [faq_controller::class, 'index'])->name('faq');
 Route::get('/about', function () {return view('about');});
 Route::get('/panduan', function () {return view('panduan');});
-
-    
-
-
